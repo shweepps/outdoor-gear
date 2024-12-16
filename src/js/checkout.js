6c35297935d0e1f9d48f96a2a8cd0af06f35acd9
shweepps <shweepps@gmail.com>
2024-12-16 14:16:34 +0200

    remo

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,21 +1,21 @@
-import { loadHeaderFooter, changeFormAction, qs } from "./utils.mjs";
-import CheckoutProcess from "./CheckoutProcess.mjs";
+// import { loadHeaderFooter, changeFormAction, qs } from "./utils.mjs";
+// import CheckoutProcess from "./CheckoutProcess.mjs";
 
-loadHeaderFooter("../index.html", "../cart/index.html");
-changeFormAction();
+// loadHeaderFooter("../index.html", "../cart/index.html");
+// changeFormAction();
 
-const myCheckout = new CheckoutProcess("so-cart", ".checkout-summary");
-myCheckout.init();
+// const myCheckout = new CheckoutProcess("so-cart", ".checkout-summary");
+// myCheckout.init();
 
-qs("#zip").addEventListener(
-  "blur",
-  myCheckout.calculateOrdertotal.bind(myCheckout),
-);
-// listening for click on the button
-qs("#checkoutSubmit").addEventListener("click", (e) => {
-  e.preventDefault();
-  const checkoutForm = document.forms.checkout;
-  const checkStatus = checkoutForm.checkValidity();
-  checkoutForm.reportValidity();
-  if (checkStatus) myCheckout.checkout();
-});
+// qs("#zip").addEventListener(
+//   "blur",
+//   myCheckout.calculateOrdertotal.bind(myCheckout),
+// );
+// // listening for click on the button
+// qs("#checkoutSubmit").addEventListener("click", (e) => {
+//   e.preventDefault();
+//   const checkoutForm = document.forms.checkout;
+//   const checkStatus = checkoutForm.checkValidity();
+//   checkoutForm.reportValidity();
+//   if (checkStatus) myCheckout.checkout();
+// });
