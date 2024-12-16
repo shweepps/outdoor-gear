16a70a84bd0fcb6c6e0536e198d96c3b2719d1dc
shweepps <shweepps@gmail.com>
2024-12-16 17:57:30 +0200

    interface done

--- src/js/cart.js
+++
@@ -1,7 +0,0 @@
-import { loadHeaderFooter, changeFormAction } from "./utils.mjs";
-import ShoppingCart from "./ShoppingCart.mjs";
-
-loadHeaderFooter("/index.html", "../cart/index.html");
-changeFormAction();
-const cart = new ShoppingCart("so-cart", ".product-list");
-cart.renderCartContents();
--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,8 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
-        cart: resolve(__dirname, "src/cart/index.html"),
-        checkout: resolve(__dirname, "src/checkout/index.html"),
-        product: resolve(__dirname, "src/product_pages/index.html"),
-        productListing: resolve(__dirname, "src/product-listing/index.html"),
+        main: resolve(__dirname, "src/index.html")
+       
       },
     },
   },
